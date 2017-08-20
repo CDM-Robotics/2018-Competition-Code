54f3037645f0a78d1e38e1185324d33e2f436b93
chersowitz <chrh02@gmail.com>
2017-08-20 16:38:45 -0700

    basic robot class

---
+++ src/main/java/frc/Robot.java
@@ -0,0 +1,15 @@
+package frc;
+import edu.wpi.first.wpilibj.IterativeRobot;
+
+
+public class Robot extends IterativeRobot {
+
+
+    @Override
+    public void robotInit() {
+        super.robotInit();
+        System.out.println("Hello World");
+
+    }
+
+}
