3270be3be274defeead898098d68ed5a4d43c09b
Egoscio <theiqboy@gmail.com>
2017-09-03 16:27:29 -0700

    Project initialized with gradle

--- src/main/java/frc/Robot.java
+++ src/main/java/frc/Robot.java
@@ -1,15 +1,13 @@
 package frc;
+
 import edu.wpi.first.wpilibj.IterativeRobot;
 
-
 public class Robot extends IterativeRobot {
-
 
     @Override
     public void robotInit() {
         super.robotInit();
         System.out.println("Hello World");
-
     }
 
 }
