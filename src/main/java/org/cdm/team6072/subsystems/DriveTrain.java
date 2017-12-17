c1a4eff6a0e4ff4a86f02d5b16defafc63aa7a10
ozdave <giTmnlk12>
2017-12-17 10:11:57 -0800

    Added comment to DriveTrain on how to configure VictorSP

--- src/main/java/org/cdm/team6072/subsystems/DriveTrain.java
+++ src/main/java/org/cdm/team6072/subsystems/DriveTrain.java
@@ -15,6 +15,9 @@ import org.cdm.team6072.SpeedControllerArray;
  */
 public class DriveTrain extends Subsystem {
 
+
+
+
     // singleton instance
     private static DriveTrain mInstance;
 
@@ -35,6 +38,53 @@ public class DriveTrain extends Subsystem {
     }
 
     private DriveControlState mDriveState;
+
+
+    /**
+     *  The PWM signal used to control the Victor SP should be between 1-2ms in duration
+     *  with a center (neutral) pulse of 1.5ms and a period between 2.9-100ms.
+     *  The PWM period is how fast the robot controller can send a new PWM pulse.
+     *  The amount of time between the rising edge of one PWM pulse to the next PWM pulse
+     *  should not be less than 2.9ms or greater than 100ms.
+     *
+     *  Calibration
+     *  The calibration of a Victor SP is essentially the scale of PWM input signal to output voltage.
+     *  Different controllers may have different “max” and “min” PWM signals that may not correspond
+     *  to the same Victor SP outputs. Calibrating the Victor SP allows it to adjust for these
+     *  differences so that a “max” signal results in a “max” output.
+     *  Calibrating can also correct issues caused by joysticks or gamepads with off-center neutral outputs.
+     *  The Victor SP’s default calibration is compatible with the roboRIO control system
+     *
+     *  Brake & Coast Modes
+     *  When a neutral PWM signal is applied to the Victor SP in Brake mode, the motor will resist rotation,
+     *  especially high speed rotation. This is accomplished by essentially shorting the motor leads, which
+     *  causes a Back Electromotive Force (Back-EMF) to resist the rotation of the motor.
+     *  Brake mode does not have any effect when the motor is not rotating, but can make a large difference
+     *  in robot behavior when used on a motor attached to a high reduction gearbox.
+     *  Brake mode does not impact performance when a non-neutral PWM signal is applied.
+     *  When a neutral PWM signal is applied to the Victor SP in Coast mode,
+     *  Back-EMF will not be generated, so the motor’s rotation will not be affected by the Victor SP.
+     *
+     *  Switching between Brake & Coast: To switch between Brake and Coast mode,
+     *  simply push the B/C CAL button at any time.
+     *  The Victor SP is in
+     *      Brake mode when the button is illuminated red and
+     *      Coast when the red light is turned off.
+     *  Brake/Coast settings are saved even if power is removed from the Victor SP.
+     *
+     * Note that the VictorSP uses the following bounds for PWM values.
+     * These values should work reasonably well for most controllers, but if users experience issues
+     * such as asymmetric behavior around the deadband or inability to saturate the controller in
+     * either direction, calibration is recommended.
+     * The calibration procedure can be found in the VictorSP User Manual available from CTRE.
+     * - 2.004ms = full "forward"
+     * - 1.52ms = the "high end" of the deadband range
+     * - 1.50ms = center of the deadband range (off)
+     * - 1.48ms = the "low end" of the deadband range
+     * - .997ms = full "reverse"
+     *
+     * ctor channel - The PWM channel that the VictorSP is attached to. 0-9 are on-board, 10-19 are on the MXP port
+     */
 
     // LEFT motors on the drive train
     VictorSP leftMotors[] = {
