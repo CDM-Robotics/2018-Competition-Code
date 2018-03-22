477faed1f8dc0e5b7f77fa4c1a267e4bdf3de7de
chersowitz <chrh02@gmail.com>
2018-03-22 11:01:11 -0700

    added test method to GoToSwitch

--- src/main/java/org/cdm/team6072/autonomous/routines/GoToSwitch.java
+++ src/main/java/org/cdm/team6072/autonomous/routines/GoToSwitch.java
@@ -3,6 +3,7 @@ package org.cdm.team6072.autonomous.routines;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import org.cdm.team6072.commands.drive.DriveDistCmd;
 import org.cdm.team6072.commands.drive.DriveTurnYawCmd;
+import org.cdm.team6072.commands.elevator.ElvMoveToSwitchCmd;
 import org.cdm.team6072.subsystems.DriveSys;
 
 public class GoToSwitch extends CommandGroup {
@@ -43,48 +44,61 @@ public class GoToSwitch extends CommandGroup {
         }
     }
 
-    public void goFromPosThreeToRight() {
+    private void goFromPosThreeToRight() {
         addSequential(new DriveDistCmd(12));
         addSequential(new DriveTurnYawCmd(-90));
+        addParallel(new ElvMoveToSwitchCmd());
         addSequential(new DriveDistCmd((float)2.95));
     }
 
     // go straight, 90 degree turn to right, straight, 90 degree turn left, straight to finish at switch
-    public void goFromPosTwoToRight() {
+    private void goFromPosTwoToRight() {
         addSequential(new DriveDistCmd(midChannelDist));
         addSequential(new DriveTurnYawCmd(90));
         addSequential(new DriveDistCmd((float)5.7));
         addSequential(new DriveTurnYawCmd(-90));
+        addParallel(new ElvMoveToSwitchCmd());
         addSequential(new DriveDistCmd(midChannelToSwitchDist));
     }
 
-    public void goFromPosOneToRight() {
+    private void goFromPosOneToRight() {
         addSequential(new DriveDistCmd(midChannelDist));
         addSequential(new DriveTurnYawCmd(90));
         addSequential(new DriveDistCmd((float)10.5));
         addSequential(new DriveTurnYawCmd(-90));
+        addParallel(new ElvMoveToSwitchCmd());
         addSequential(new DriveDistCmd(midChannelToSwitchDist));
     }
 
-    public void goFromPosThreeToLeft() {
+    private void goFromPosThreeToLeft() {
         addSequential(new DriveDistCmd(midChannelDist));
         addSequential(new DriveTurnYawCmd(-90));
         addSequential(new DriveDistCmd((float)10.5));
         addSequential(new DriveTurnYawCmd(90));
+        addParallel(new ElvMoveToSwitchCmd());
         addSequential(new DriveDistCmd(midChannelToSwitchDist));
     }
 
-    public void goFromPosTwoToLeft() {
+    private void goFromPosTwoToLeft() {
         addSequential(new DriveDistCmd(midChannelDist));
         addSequential(new DriveTurnYawCmd(-90));
         addSequential(new DriveDistCmd((float)5.7));
         addSequential(new DriveTurnYawCmd(90));
+        addParallel(new ElvMoveToSwitchCmd());
         addSequential(new DriveDistCmd(midChannelToSwitchDist));
     }
 
-    public void goFromPosOnetoLeft() {
+    private void goFromPosOnetoLeft() {
         addSequential(new DriveDistCmd(12));
         addSequential(new DriveTurnYawCmd(90));
+        addParallel(new ElvMoveToSwitchCmd());
         addSequential(new DriveDistCmd((float)2.95));
+    }
+
+    public void testSwitch() {
+        addSequential(new DriveDistCmd(1));
+        addSequential(new DriveTurnYawCmd(90));
+        addParallel(new ElvMoveToSwitchCmd());
+        addSequential(new DriveDistCmd(1));
     }
 }
